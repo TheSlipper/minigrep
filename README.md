39f96a60e93b4040941b2be3fd25a21e008b458c
Slipper <slipper@pop-os.localdomain>
2020-06-11 17:06:53 +0200

    refactoring to improve modularity and error handling

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # minigrep
+
+A simple grep program implementation that I made while studying the rust language from "The Rust Programming Language" book.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::process;
+
+use minigrep_lib;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = minigrep_lib::Config::new(&args).unwrap_or_else(|err| {
+        println!("Encountered a problem while parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = minigrep_lib::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
