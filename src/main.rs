884ec37076c4141b40590f7da9243bdc1265f80a
TheSlipper <korneldomeradzki1@gmail.com>
2020-06-13 10:46:59 +0200

    Environment variable support and a streaming fix

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use minigrep_lib;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = minigrep_lib::Config::new(&args).unwrap_or_else(|err| {
-        println!("Encountered a problem while parsing arguments: {}", err);
+        eprintln!("Encountered a problem while parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep_lib::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
