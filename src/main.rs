349e507fa2aebdca6af25393c2c532e728fb8c45
TheSlipper <korneldomeradzki1@gmail.com>
2020-06-20 15:33:44 +0200

    Incorporated closures and iterators

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ use std::process;
 use minigrep_lib;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = minigrep_lib::Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Encountered a problem while parsing arguments: {}", err);
+    // let args: Vec<String> = env::args().collect();
+    // let config = minigrep_lib::Config::new(&args).unwrap_or_else(|err| {
+    //     eprintln!("Encountered a problem while parsing arguments: {}", err);
+    //     process::exit(1);
+    // });
+    let config = minigrep_lib::Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
